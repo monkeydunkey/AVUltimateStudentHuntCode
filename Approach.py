ba3ffb2a57f5b051362cb97751ef0d68e264e2f5
monkeydunkey <bhushan.shashank93@gmail.com>
2016-09-27 12:26:07 -0700

    tried xgboost

--- Approach.py
+++ Approach.py
@@ -4,49 +4,47 @@ import matplotlib.pyplot as plt
 from scipy.stats import skew
 from sklearn.linear_model import LassoCV
 from sklearn.cross_validation import cross_val_score
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LinearRegression, Ridge
+import xgboost as xgb
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
+
+train['Location_Type'] = train['Location_Type'].astype(str)
+test['Location_Type'] = test['Location_Type'].astype(str)
 
 all_data = pd.concat((train.loc[:, 'Park_ID':'Location_Type'],
                       test.loc[:, 'Park_ID':'Location_Type']))
 all_data['Day'] = all_data.Date.str.split('-').str[0].astype(int)
 all_data['Month'] = all_data.Date.str.split('-').str[1].astype(int)
-all_data['Year'] = all_data.Date.str.split('-').str[2].astype(int)
-all_data = all_data.drop('Date', axis = 1)
+all_data = all_data.drop('Date', axis=1)
 
 train['Day'] = train.Date.str.split('-').str[0].astype(int)
 train['Month'] = train.Date.str.split('-').str[1].astype(int)
-train['Year'] = train.Date.str.split('-').str[2].astype(int)
-train = train.drop('Date', axis = 1)
+train = train.drop('Date', axis=1)
 
-'''
-li = all_data.Date.str.split('-').tolist()
-for i, j in enumerate(li):
-    li[i] = int("".join(reversed(j)))
-all_data['Date'] = pd.Series(li)
+numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
 
-li = train.Date.str.split('-').tolist()
-for i, j in enumerate(li):
-    li[i] = int("".join(reversed(j)))
-train['Date'] = pd.Series(li)
-'''
 # For right skewed data
-skewed_feats = train.apply(lambda x: skew(x.dropna()))
+skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna()))
 skewed_feats = skewed_feats[skewed_feats > 0.8]
 skewed_feats = skewed_feats.index
 
 all_data[skewed_feats] = np.log1p(all_data[skewed_feats])
 
 # For left skewed data
-skewed_feats_left = train.apply(lambda x: skew(x.dropna()))
+skewed_feats_left = train[numeric_feats].apply(lambda x: skew(x.dropna()))
 skewed_feats_left = skewed_feats_left[skewed_feats_left < -0.8]
 skewed_feats_left = skewed_feats_left.index
 
 all_data[skewed_feats_left] = np.power(all_data[skewed_feats_left], 3)
+
+# Caculating the dummy variables
+all_data = pd.get_dummies(all_data)
+train = pd.get_dummies(train)
+test = pd.get_dummies(test)
 
 all_data = all_data.fillna(all_data.median())
 X_train = all_data[:train.shape[0]]
@@ -68,8 +66,21 @@ preds = model.predict(X_test)
 model = RandomForestRegressor()
 model.fit(X_train, y)
 preds = model.predict(X_test)
+
+
+model = GradientBoostingRegressor(min_samples_split=800, min_samples_leaf=50,
+                                  max_depth=9, max_features='sqrt',
+                                  subsample=0.8, random_state=10)
+model.fit(X_train, y)
+preds = model.predict(X_test)
+
+
+model = AdaBoostRegressor()
+model.fit(X_train, y)
+preds = model.predict(X_test)
+
 '''
-model = GradientBoostingRegressor()
+model = xgb.XGBRegressor()
 model.fit(X_train, y)
 preds = model.predict(X_test)
 
