adbf336eae64885fce218efdb310f47f6e18b496
monkeydunkey <bhushan.shashank93@gmail.com>
2016-10-03 18:40:31 -0700

    final commit for now

--- Approach.py
+++ Approach.py
@@ -7,16 +7,21 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.grid_search import GridSearchCV
 import xgboost as xgb
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-train['Location_Type'] = train['Location_Type'].astype(str)
-test['Location_Type'] = test['Location_Type'].astype(str)
+# Coverting Location type to str to turn it into an object
 
-all_data = pd.concat((train.loc[:, 'Park_ID':'Location_Type'],
-                      test.loc[:, 'Park_ID':'Location_Type']))
+train = train.drop('Location_Type', axis=1)
+test = test.drop('Location_Type', axis=1)
+# test['Location_Type'] = test['Location_Type'].astype(str)
+
+all_data = pd.concat((train.loc[:, 'Park_ID':'Min_Moisture_In_Park'],
+                      test.loc[:, 'Park_ID':'Min_Moisture_In_Park']))
+# extracting month and day information from the date
 all_data['Day'] = all_data.Date.str.split('-').str[0].astype(int)
 all_data['Month'] = all_data.Date.str.split('-').str[1].astype(int)
 all_data = all_data.drop('Date', axis=1)
@@ -41,48 +46,32 @@ skewed_feats_left = skewed_feats_left.index
 
 all_data[skewed_feats_left] = np.power(all_data[skewed_feats_left], 3)
 
-# Caculating the dummy variables
+
+all_data = all_data.fillna(all_data.median())
+'''
+# Caculating the dummy variables - For location type
 all_data = pd.get_dummies(all_data)
 train = pd.get_dummies(train)
 test = pd.get_dummies(test)
+'''
 
-all_data = all_data.fillna(all_data.median())
 X_train = all_data[:train.shape[0]]
 X_test = all_data[train.shape[0]:]
 y = train.Footfall
-'''
-model = LassoCV(alphas=[1, 0.1, 0.001, 0.0005], max_iter = 2500)
-model.fit(X_train, y)
-preds = model.predict(X_test)
-
-model = LinearRegression()
-model.fit(X_train, y)
-preds = model.predict(X_test)
-
-model = Ridge(10)
-model.fit(X_train, y)
-preds = model.predict(X_test)
-
-model = RandomForestRegressor()
-model.fit(X_train, y)
-preds = model.predict(X_test)
 
 
-model = GradientBoostingRegressor(min_samples_split=800, min_samples_leaf=50,
-                                  max_depth=9, max_features='sqrt',
+def rmse_cv(model):
+    rmse = -cross_val_score(model, X_train, y, scoring="mean_squared_error",
+                            cv=5)
+    return rmse
+
+model = GradientBoostingRegressor(n_estimators=800, min_samples_leaf=50,
+                                  loss='huber', learning_rate=0.04,
+                                  max_depth=14, max_features='sqrt',
                                   subsample=0.8, random_state=10)
+
 model.fit(X_train, y)
 preds = model.predict(X_test)
 
-
-model = AdaBoostRegressor()
-model.fit(X_train, y)
-preds = model.predict(X_test)
-
-'''
-model = xgb.XGBRegressor()
-model.fit(X_train, y)
-preds = model.predict(X_test)
-
-solution = pd.DataFrame({"ID": test.ID, "Footfall": preds})
+solution = pd.DataFrame({"ID": test.ID, "Footfall": np.round(preds)})
 solution.to_csv("Try2.csv", index=False)
